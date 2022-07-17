9ac46847d2b4f7bea2b6add42b1aa402846c839d
apple-yagi <applesryu@gmail.com>
2022-07-17 23:16:15 +0900

    Rearchitect directory

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,56 @@
+import { $getRoot, $getSelection } from "lexical";
+import type { NextPage } from "next";
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { PlainTextPlugin } from "@lexical/react/LexicalPlainTextPlugin";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import { EditorState } from "lexical";
+import { useEffect } from "react";
+
+const Home: NextPage = () => {
+  const initialConfig = {
+    namespace: "MyEditor",
+    onError,
+  };
+
+  function onChange(editorState: EditorState) {
+    editorState.read(() => {
+      // Read the contents of the EditorState here.
+      const root = $getRoot();
+      const selection = $getSelection();
+
+      console.log(root, selection);
+    });
+  }
+
+  function MyCustomAutoFocusPlugin() {
+    const [editor] = useLexicalComposerContext();
+
+    useEffect(() => {
+      // Focus the editor when the effect fires!
+      editor.focus();
+    }, [editor]);
+
+    return null;
+  }
+
+  function onError(error: Error) {
+    console.error(error);
+  }
+
+  return (
+    <LexicalComposer initialConfig={initialConfig}>
+      <PlainTextPlugin
+        contentEditable={<ContentEditable />}
+        placeholder={<div>Enter some text...</div>}
+      />
+      <OnChangePlugin onChange={onChange} />
+      <HistoryPlugin />
+      <MyCustomAutoFocusPlugin />
+    </LexicalComposer>
+  );
+};
+
+export default Home;
